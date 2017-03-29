929641725b5e7d708dc6ea8257544469b1e473ca
Josh Gachnang <josh@servercobra.com>
2017-03-29 09:39:52 -0500

    Add Ionic Deploy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Auth, Deploy, User } from '@ionic/cloud-angular';
 
 import { ApiService, Score } from '../../services/api';
 import { TeamScorePage } from '../scores/team';
@@ -13,9 +14,11 @@ import { ScoresPage } from '../scores/scores';
 export class HomePage {
   public scores: Score[] = [];
 
-  constructor(public navCtrl: NavController, public api: ApiService) {}
+  constructor(public navCtrl: NavController, public api: ApiService, public deploy: Deploy, public authA: Auth,
+              public userA: User) {}
 
   ionViewDidLoad() {
+    this.checkForNewVersion();
     this.api.fetchScores().subscribe((scores: Score[]) => {
       console.debug('Fetched scores: ', scores);
       this.scores = scores;
@@ -32,4 +35,21 @@ export class HomePage {
     console.log('all scores');
   }
 
+  private checkForNewVersion() {
+    console.debug('checking for new app version');
+    if (this.authA.isAuthenticated()) {
+
+      console.log("checking if we shoudl set channel", this.userA);
+    }
+    this.deploy.check().then((snapshotAvailable: boolean) => {
+      if (snapshotAvailable) {
+        console.info('new version available')
+        this.deploy.download().then(() => {
+            return this.deploy.extract();
+        });
+      } else {
+        console.debug('no new version');
+      }
+    });
+  }
 }
